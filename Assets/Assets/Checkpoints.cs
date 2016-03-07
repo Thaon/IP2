0b342701afbfd37386798e4c64fe0464c4ab0c9c
Thaon <gabriele.maddaloni@gmail.com>
2016-03-07 17:25:21 +0000

    Checkpoints reworked

--- Assets/Assets/Checkpoints.cs
+++ Assets/Assets/Checkpoints.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace Ip2
 {
     public class Checkpoints : MonoBehaviour {
 
+        public GameObject m_p1UI;
+        public GameObject m_p2UI;
+        public GameObject m_p3UI;
+        public GameObject m_p4UI;
+
+        Text m_p1text;
+        Text m_p2text;
+        Text m_p3text;
+        Text m_p4text;
 
 
         // Use this for initialization
         void Start() {
-
+            m_p1text = m_p1UI.GetComponent<Text>();
+            m_p2text = m_p2UI.GetComponent<Text>();
+            m_p3text = m_p3UI.GetComponent<Text>();
+            m_p4text = m_p4UI.GetComponent<Text>();
         }
 
         // Update is called once per frame
@@ -24,6 +37,7 @@ namespace Ip2
                 {
                     case 1:
                         data.player1Score++;
+                        m_p1text.text = data.player1Score.ToString();
                         if (data.player1Score > 4)
                         {
                             data.m_winningPlayer = 1;
@@ -33,6 +47,7 @@ namespace Ip2
 
                     case 2:
                         data.player2Score++;
+                        m_p2text.text = data.player2Score.ToString();
                         if (data.player2Score > 4)
                         {
                             data.m_winningPlayer = 2;
@@ -42,6 +57,7 @@ namespace Ip2
 
                     case 3:
                         data.player3Score++;
+                        m_p3text.text = data.player3Score.ToString();
                         if (data.player2Score > 4)
                         {
                             data.m_winningPlayer = 3;
@@ -51,6 +67,7 @@ namespace Ip2
 
                     case 4:
                         data.player4Score++;
+                        m_p4text.text = data.player4Score.ToString();
                         if (data.player4Score > 4)
                         {
                             data.m_winningPlayer = 4;
